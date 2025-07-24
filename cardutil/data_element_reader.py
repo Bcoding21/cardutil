4b3e1bb9751577ffc6ca2508e425d0821b94e507
brandoncole <brandoncole673@gmail.com>
2025-07-24 18:39:34 -0400

    Refactor data element reading logic to use IpmMessage and IpmMessageIterator classes

--- cardutil/data_element_reader.py
+++ cardutil/data_element_reader.py
@@ -72,93 +72,192 @@ Example Input and Output:
         ]
 """
 
+from collections.abc import Iterator
+from dataclasses import dataclass
 from functools import partial
+from io import BytesIO
 from typing import BinaryIO, Callable
 
 
 
-def create_data_element_reader(file: BinaryIO, bit_config: dict) -> Callable[[bytes], list[bytes | None]]:
-    data_element_readers = _create_reader_index(file, bit_config)
+@dataclass
+class IpmMessage:
+   mti: bytes
+   bitmap: bytes
+   data_elements: list[bytes | None]
+
+
+class _StrictBinaryIO(BinaryIO):
+
+    def __init__(self, file: BinaryIO):
+        self._file = file
+        super().__init__()
+
+    def read(self, size: int) -> bytes:
+        data = self._file.read(size)
+
+        if len(data) < size:
+            raise EOFError("Unexpected end of file")
+
+        return data
     
-    def read_data_elements(bit_map: bytes) -> list[bytes | None]:
-        set_bit_indices = _determine_set_bit_indices(bit_map)
-        data_elements = [None] * (len(data_element_readers))
+    def seek(self, offset: int | None, whence: int = 0) -> int:
+        position = self._file.seek(offset or 0, whence)
+        if position < 0:
+            raise ValueError("Seek position cannot be negative")
+        return position
+    
+
+    def tell(self) -> int:
+        position = self._file.tell()
+        if position < 0:
+            raise ValueError("Tell position cannot be negative")
+        return position
+
+
+class IpmMessageIterator(Iterator[IpmMessage]):
+    MTI_LENGTH = 4
+    BITMAP_LENGTH = 16
+
+    def __init__(self, file: BinaryIO, bit_config: dict):
+        self.file = _StrictBinaryIO(file)
+        self.bit_config = bit_config
+        self.data_element_readers = self._create_reader_index()
+
+    def __iter__(self) -> 'IpmMessageIterator':
+        return self
+    
+    def __next__(self) -> IpmMessage:
+
+        if self._is_end_of_file():
+            raise StopIteration("End of file reached")
+
+        mti = self.file.read(self.MTI_LENGTH)
+        bitmap = self.file.read(self.BITMAP_LENGTH)
+        data_elements = self._read_data_elements(bitmap)
+        return IpmMessage(mti=mti, bitmap=bitmap, data_elements=data_elements)
+    
+    def _is_end_of_file(self) -> bool:
+        current_position = self.file.tell()
+        try:
+            self.file.read(1)
+        except EOFError:
+            return True
+        finally:
+            self.file.seek(current_position)
+
+    def _read_data_elements(self, bit_map: bytes) -> list[bytes | None]:
+        set_bit_indices = self._determine_set_bit_indices(bit_map)
+        data_elements = [None] * len(self.data_element_readers)
 
         for bit_index in set_bit_indices:
-
-            if data_element_readers[bit_index] is not None:
-                read_data_element = data_element_readers[bit_index]
+            if self.data_element_readers[bit_index] is not None:
+                read_data_element = self.data_element_readers[bit_index]
                 data_elements[bit_index] = read_data_element()
 
         return data_elements
-
-    return read_data_elements
-
-
-def _create_reader_index(file: BinaryIO, bit_config: dict) -> list[Callable[[], bytes]]:
-    max_bit = max(int(bit) for bit in bit_config.keys())
-    reader_index = [None] * (max_bit + 1)
-
-    for bit, bit_details in bit_config.items():
-
-        bit_index = int(bit)
-        field_type = bit_details["field_type"]
-        field_length = bit_details.get("field_length", 0)
-
-        if field_type == "FIXED":
-            reader_index[bit_index] = _create_fixed_width_reader(file, field_length)
-
-        elif field_type == "LLVAR":
-            reader_index[bit_index] = _create_llvar_field_reader(file)
-
-        elif field_type == "LLLVAR":
-            reader_index[bit_index] = _create_lllvar_field_reader(file)
-
-        else:
-            raise ValueError(f"Unknown field type: {field_type}")
     
-    return reader_index
+    def _create_reader_index(self) -> list[Callable[[], bytes]]:
+        max_bit = max(int(bit) for bit in self.bit_config.keys())
+        reader_index = [None] * (max_bit + 1)
 
-def _create_fixed_width_reader(file: BinaryIO, length: int) -> Callable[[], bytes]:
-   return partial(file.read, length)
+        for bit, bit_details in self.bit_config.items():
+            bit_index = int(bit)
+            field_type = bit_details["field_type"]
+            field_length = bit_details.get("field_length", 0)
 
-def _create_variable_length_reader(file: BinaryIO, length: int) -> Callable[[], bytes]:
+            if field_type == "FIXED":
+                reader_index[bit_index] = self._create_fixed_width_reader(field_length)
+            elif field_type == "LLVAR":
+                reader_index[bit_index] = self._create_llvar_field_reader()
+            elif field_type == "LLLVAR":
+                reader_index[bit_index] = self._create_lllvar_field_reader()
+            else:
+                raise ValueError(f"Unknown field type: {field_type}")
 
-    def reader(length: int = length) -> bytes:
-        length_bytes = file.read(length)
+        return reader_index
 
-        if len(length_bytes) < length:
-            raise ValueError(
-                f"Expected to read {length} bytes for length prefix, but got {len(length_bytes)} bytes."
-            )
+    def _create_fixed_width_reader(self, length: int) -> Callable[[], bytes]:
+        return partial(self.file.read, length)
 
-        try:
-            length = int(length_bytes)
-        except ValueError:
-            raise ValueError(f"Invalid length prefix: {length_bytes!r} is not a valid integer.")
+    def _create_variable_length_reader(self, length: int) -> Callable[[], bytes]:
 
-        data = file.read(length)
+        def reader(length: int = length) -> bytes:
+            length_bytes = self.file.read(length)
 
-        if len(data) != length:
-            raise ValueError(
-                f"Expected to read {length} bytes of data, but got {len(data)} bytes."
-            )
+            if len(length_bytes) < length:
+                raise ValueError(
+                    f"Expected to read {length} bytes for length prefix, but got {len(length_bytes)} bytes."
+                )
 
-        return data
+            try:
+                length = int(length_bytes)
+            except ValueError:
+                raise ValueError(f"Invalid length prefix: {length_bytes!r} is not a valid integer.")
 
-    return reader
+            data = self.file.read(length)
 
-def _create_llvar_field_reader(file: BinaryIO) -> Callable[[], bytes]:
-    return _create_variable_length_reader(file, 2)
+            if len(data) != length:
+                raise ValueError(
+                    f"Expected to read {length} bytes of data, but got {len(data)} bytes."
+                )
 
-def _create_lllvar_field_reader(file: BinaryIO) -> Callable[[], bytes]:
-    return _create_variable_length_reader(file, 3)
+            return data
 
-def _determine_set_bit_indices(bit_map: bytes) -> list[int]:
-    set_bits = []
-    for i, byte in enumerate(bit_map):
-        for j in range(8):
-            if byte & (1 << (7 - j)):
-                set_bits.append(i * 8 + j + 1)
-    return set_bits
+        return reader
 
+    def _create_llvar_field_reader(self) -> Callable[[], bytes]:
+        return self._create_variable_length_reader(2)
+
+    def _create_lllvar_field_reader(self) -> Callable[[], bytes]:
+        return self._create_variable_length_reader(3)
+
+    def _determine_set_bit_indices(self, bit_map: bytes) -> list[int]:
+        set_bits = []
+        for i, byte in enumerate(bit_map):
+            for j in range(8):
+                if byte & (1 << (7 - j)):
+                    set_bits.append(i * 8 + j + 1)
+        return set_bits
+
+
+class RDWIpmMessageIterator(IpmMessageIterator):
+    RDW_LENGTH = 4
+
+    def __init__(self, file: BinaryIO, bit_config: dict):
+        super().__init__(file, bit_config)
+
+    def __next__(self) -> IpmMessage:
+        raw_length = self.file.read(self.RDW_LENGTH)
+        length = int.from_bytes(raw_length, byteorder='big')
+
+        if length == 0:
+            raise StopIteration("End of file reached")
+
+        return super().__next__()
+
+
+class BlockedReader(BinaryIO):
+
+    def __init__(self, file: BinaryIO, block_size: int = 1014):
+        self._file = file
+        self._block_size = block_size
+        self._buffer = BytesIO()
+
+    def read(self, total_bytes: int) -> bytes:
+       self._fill_buffer_if_needed(total_bytes)
+       return self._buffer.read(total_bytes)
+
+    def _fill_buffer_if_needed(self, total_bytes: int) -> None:
+
+         while self._get_bytes_left_in_buffer() < total_bytes:
+            block = self._file.read(self._block_size)
+
+            if not block:
+                break
+
+            block = block[:self._block_size - 2]
+            self._buffer.write(block)
+
+    def _get_bytes_left_in_buffer(self) -> int:
+        return self._buffer.getbuffer().nbytes - self._buffer.tell()
--- tests/test_data_element_reader.py
+++ tests/test_data_element_reader.py
@@ -1,94 +1,140 @@
 from io import BytesIO
 
 import pytest
-from cardutil.data_element_reader import create_data_element_reader
+from cardutil.data_element_reader import IpmMessage, IpmMessageIterator
 
 
 def test_create_reader__fixed_width_element__reads_correctly():
-    file = BytesIO(b"12345")
+    bit_map = _make_bitmap([1])
+    ipm_message = (b"1240" + bit_map + b"12345")
+    file = BytesIO(ipm_message)
     config = {"1": {"field_type": "FIXED", "field_length": 5}}
-    bitmap = _make_bitmap([1])
+    iterator = IpmMessageIterator(file, config)
 
-    read_data_elements = create_data_element_reader(file, config)
-    data_elements = read_data_elements(bitmap)
+    data_elements = list(iterator)
 
-    assert data_elements == [None, b"12345"]
+    assert data_elements == [
+        IpmMessage(
+            mti=b"1240",
+            bitmap=bit_map,
+            data_elements=[None, b"12345"]
+        )
+    ]
 
 
 def test_create_reader__llvar_element__reads_correctly():
-    file = BytesIO(b"05hello")
-    config = {"2": {"field_type": "LLVAR"}}
-    bitmap = _make_bitmap([2])
+    bit_map = _make_bitmap([1])
+    ipm_message = (b"1240" + bit_map + b"05hello")
+    file = BytesIO(ipm_message)
+    config = {"1": {"field_type": "LLVAR"}}
+    iterator = IpmMessageIterator(file, config)
 
-    read_data_elements = create_data_element_reader(file, config)
-    data_elements = read_data_elements(bitmap)
+    data_elements = list(iterator)
 
-    assert data_elements == [None, None, b"hello"]
+    assert data_elements == [
+        IpmMessage(
+            mti=b"1240",
+            bitmap=bit_map,
+            data_elements=[None, b"hello"]
+        )
+    ]
 
 
 def test_create_reader__lllvar_element__reads_correctly():
-    file = BytesIO(b"005hello")
-    config = {"3": {"field_type": "LLLVAR"}}
-    bitmap = _make_bitmap([3])
+    bit_map = _make_bitmap([1])
+    ipm_message = (b"1240" + bit_map + b"005hello")
+    file = BytesIO(ipm_message)
+    config = {"1": {"field_type": "LLLVAR"}}
+    iterator = IpmMessageIterator(file, config)
 
-    read_data_elements = create_data_element_reader(file, config)
-    data_elements = read_data_elements(bitmap)
+    data_elements = list(iterator)
 
-    assert data_elements == [None, None, None, b"hello"]
+    assert data_elements == [
+        IpmMessage(
+            mti=b"1240",
+            bitmap=bit_map,
+            data_elements=[None, b"hello"]
+        )
+    ]
 
 
 def test_create_reader__multiple_elements__reads_correctly():
-    file = BytesIO(b"hello05hello005hello")
+    bit_map = _make_bitmap([1, 2, 3])
+    ipm_message = (b"1240" + bit_map + b"hello05hello005hello")
+    file = BytesIO(ipm_message)
     config = {
         "1": {"field_type": "FIXED", "field_length": 5},
         "2": {"field_type": "LLVAR"},
         "3": {"field_type": "LLLVAR"},
     }
-    bitmap = _make_bitmap([1, 2, 3])
+    iterator = IpmMessageIterator(file, config)
 
-    read_data_elements = create_data_element_reader(file, config)
-    data_elements = read_data_elements(bitmap)
+    data_elements = list(iterator)
 
-    assert data_elements == [None, b"hello", b"hello", b"hello"]
+    assert data_elements == [
+        IpmMessage(
+            mti=b"1240",
+            bitmap=bit_map,
+            data_elements=[None, b"hello", b"hello", b"hello"]
+        )
+    ]
 
+
+def test_iterator__multiple_messages():
+    bit_map1 = _make_bitmap([1, 2])
+    ipm_message1 = (b"1240" + bit_map1 + b"hello05world")
+    
+    bit_map2 = _make_bitmap([1, 3])
+    ipm_message2 = (b"1241" + bit_map2 + b"world004test")
+
+    file = BytesIO(ipm_message1 + ipm_message2)
+    config = {
+        "1": {"field_type": "FIXED", "field_length": 5},
+        "2": {"field_type": "LLVAR"},
+        "3": {"field_type": "LLLVAR"},
+    }
+    iterator = IpmMessageIterator(file, config)
+
+    data_elements = list(iterator)
+
+    assert len(data_elements) == 2
+    assert data_elements[0].mti == b"1240"
+    assert data_elements[0].data_elements == [None, b"hello", b"world", None]
+    assert data_elements[1].mti == b"1241"
+    assert data_elements[1].data_elements == [None, b"world", None, b"test"]
 
 def test_create_reader__llvar_element__raises_error_on_short_length_prefix():
-    file = BytesIO(b"0")
+    file = BytesIO(b"1240" + _make_bitmap([2]) + b"1hello")
     config = {"2": {"field_type": "LLVAR"}}
-    bitmap = _make_bitmap([2])
-
-    read_data_elements = create_data_element_reader(file, config)
+    
+    iterator = IpmMessageIterator(file, config)
 
     with pytest.raises(
-        ValueError, match="Expected to read 2 bytes for length prefix, but got 1 bytes."
+        ValueError, match="Invalid length prefix: b'1h' is not a valid integer."
     ):
-        read_data_elements(bitmap)
+        list(iterator)
 
 
 def test_create_reader__llvar_element__raises_error_on_invalid_length_prefix():
-    file = BytesIO(b"xxhello")
-    config = {"2": {"field_type": "LLVAR"}}
-    bitmap = _make_bitmap([2])
-
-    read_data_elements = create_data_element_reader(file, config)
+    file = BytesIO(b"1240" + _make_bitmap([2]) + b"01hello")
+    config = {"2": {"field_type": "LLLVAR"}}
+    
+    iterator = IpmMessageIterator(file, config)
 
     with pytest.raises(
-        ValueError, match="Invalid length prefix: b'xx' is not a valid integer."
+        ValueError, match="Invalid length prefix: b'01h' is not a valid integer."
     ):
-        read_data_elements(bitmap)
+        list(iterator)
 
 
 def test_create_reader__llvar_element__raises_error_on_short_data():
-    file = BytesIO(b"10hello")
+    file = BytesIO(b"1240" + _make_bitmap([2]) + b"10hello")
     config = {"2": {"field_type": "LLVAR"}}
-    bitmap = _make_bitmap([2])
+    
+    iterator = IpmMessageIterator(file, config)
 
-    read_data_elements = create_data_element_reader(file, config)
-
-    with pytest.raises(
-        ValueError, match="Expected to read 10 bytes of data, but got 5 bytes."
-    ):
-        read_data_elements(bitmap)
+    with pytest.raises(EOFError, match="Unexpected end of file"):
+        list(iterator)
 
 
 def _make_bitmap(indices: list[int]) -> bytes:
